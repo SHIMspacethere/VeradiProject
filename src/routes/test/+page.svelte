ce92240f301b05170aaa217c2cde79cb45239473
심현섭 <89609620+SHIMspacethere@users.noreply.github.com>
2023-01-09 04:03:06 +0900

    tailwind 적용

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="tw-text-3xl tw-font-bold tw-underline">
+  Hello world!fdfd
+</h1>
---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+	// add this section
+	prefix: 'tw-',
+	content: ['./src/**/*.html', './src/**/*.svelte'],
+	theme: {
+		screens: {
+			sm: '576px',
+			md: '768px',
+			lg: '992px',
+			xl: '1200px'
+		},
+		fontSize: {
+			xs: '.750rem',
+			sm: '.850rem',
+			base: '.9375rem',
+			md: '.9375rem',
+			lg: '1rem',
+			xl: '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.875rem',
+			'4xl': '2rem',
+			'5xl': '2.25rem',
+			'6xl': '3rem',
+			'7xl': '4rem'
+		},
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: [],
+	important: true
+};
